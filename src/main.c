fa13cd8fc69609e9fcae372d3bef2ee0aab895c9
Loraine GAULTIER <lgaultie@e1r5p1.42.fr>
2019-02-14 11:49:41 +0100

    changement du main pour enlever un leaks en cas d'error

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amamy <amamy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lgaultie <lgaultie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 15:12:08 by lgaultie          #+#    #+#             */
-/*   Updated: 2019/02/10 21:00:30 by amamy            ###   ########.fr       */
+/*   Updated: 2019/02/14 11:46:19 by lgaultie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,14 @@ int		ft_usage(void)
 	return (0);
 }
 
-int		ft_error(void)
+int		ft_error(char **map)
+{
+	ft_free_map(map);
+	ft_putstr("error\n");
+	return (0);
+}
+
+int		ft_error1(void)
 {
 	ft_putstr("error\n");
 	return (0);
@@ -42,11 +49,11 @@ int		main(int argc, char **argv)
 	{
 		if (((((fd = open(argv[1], O_RDONLY)) == -1) \
 		|| ((str = ft_read_file(fd)) == NULL))))
-			return (ft_error());
+			return (ft_error1());
 		len = ft_strlen(str);
 		if (((((tetris = (ft_check_and_letter(str, tetris))) == NULL) \
 		|| (ft_create_map(len, map)) != 0)))
-			return (ft_error());
+			return (ft_error(map));
 		ft_solve(tetris, tetris, map);
 	}
 	ft_display_and_free_map(map, tetris);
